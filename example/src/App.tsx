96efc781da01e80752c7bebe1c13bb7607a51b11
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2023-09-28 08:22:56 -0700

    example: add usage example for SwipeableElement

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,94 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import { SwipeableElement } from 'react-native-swipeable-element';
+import type { SwipeableElementProps } from '../../src/SwipeableElement/types';
+function mockApiCall() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('');
+    }, 1000);
+  });
+}
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-swipeable-element';
+function CheckMark() {
+  return (
+    <Image
+      source={require('../assets/check-mark.png')}
+      style={styles.finishedIcon}
+      resizeMode="contain"
+    />
+  );
+}
 
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+function ElementToSwipe() {
+  return (
+    <Image
+      source={require('../assets/up-arrow.png')}
+      style={styles.icon}
+      resizeMode="contain"
+    />
+  );
+}
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
+
+  const onSwipeEnd: SwipeableElementProps['onSwipeComplete'] = async (e) => {
+    console.log('onSwipeComplete: ', e);
+    if (e.isTopReached) {
+      try {
+        setIsLoading(true);
+        await mockApiCall();
+        setIsFinished(true);
+        setTimeout(() => {
+          setIsFinished(false);
+        }, 2000);
+      } catch (error) {
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
+    <View style={styles.root}>
+      <SwipeableElement
+        isLoading={isLoading}
+        isFinished={isFinished}
+        styleProp={{
+          container: styles.container,
+          swipeableWrapper: styles.swipeableWrapper,
+        }}
+        onSwipeComplete={onSwipeEnd}
+        FinishedComponent={CheckMark}
+        Element={ElementToSwipe}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    alignItems: 'center',
+    backgroundColor: 'powderblue',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  container: {
+    height: 80,
+    aspectRatio: 1,
+    borderRadius: 100,
+    backgroundColor: '#333',
+    alignItems: 'center',
+  },
+  swipeableWrapper: { marginTop: 'auto', marginBottom: 6 },
+  icon: { tintColor: 'white', height: 40 },
+  finishedIcon: {
+    tintColor: 'white',
+    height: 40,
+    marginTop: 'auto',
+    marginBottom: 'auto',
   },
 });
+export default App;
